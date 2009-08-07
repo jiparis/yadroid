6d935ec9b5e4598ddccd95989b106541cf815ead
jloramas <jloramas@25cf9160-3d47-0410-9de4-f1fe730f28bb>
2009-08-07 17:27:18 +0000

    Multiples cambios para pruebas:     - Transformación de valores de ángulos x e y para que tengan un comportamiento exponencial     - Wrapper de OpenGL para poder extraer las matrices y realizar la proyección de vértices     - Pintado de textos estáticos y textos asociados a un vértice     - Rotación independiente de la brújula y del palo, y usando quaterniones     - Rotación de la brújula en las 3 direcciones     - Rotación del palo sólo en la x     - Implementación del método resize para capturar las dimensiones del view

--- WorldShadows/src/org/jiagjl/ShadowsView.java
+++ WorldShadows/src/org/jiagjl/ShadowsView.java
@@ -4,9 +4,13 @@ import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
 
+import org.jiagjl.drawtext.LabelMaker;
+import org.jiagjl.drawtext.matrix.MatrixTrackingGL;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Paint;
 import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.util.AttributeSet;
@@ -59,6 +63,33 @@ public class ShadowsView extends GLBase {
 	float lightDiffuse[]= { 1.0f, 1.0f, 1.0f, 1.0f };	// Diffuse Light Values 
 	float lightPosition[]= { 0.0f, 0.0f, 2.0f, 1.0f };	// Light Position 	
 
+	
+	LabelMaker mLabels;
+	Paint mLabelPaint;
+	int mLabelTest;
+	int mLabelNE, mLabelSE, mLabelNW, mLabelSW;
+	float[] qm = new float[16];
+	
+	static float DEGREES_MIN = 0.0f;
+	static float DEGREES_MAX = 90.0f;
+	static float FX_MIN = 0.0f;
+	static float FX_MAX = 2.0f;
+	static float DEGREES_TO_FX_FACTOR = Utils.scaleFactor( DEGREES_MIN, DEGREES_MAX, FX_MIN, FX_MAX ); 
+	
+	static float FX_RES_MIN = fx(FX_MIN);
+	static float FX_RES_MAX = fx(FX_MAX);
+	static float FX_RES_TO_DEGREES_FACTOR = Utils.scaleFactor( FX_RES_MIN, FX_RES_MAX, DEGREES_MIN, DEGREES_MAX ); 
+
+
+	//Mapea los grados para que sigan una progresión exponencial en lugar de lineal
+	static public float softenDegrees( float angle ) {
+		return Utils.scale( fx( Utils.scale( angle, DEGREES_TO_FX_FACTOR, DEGREES_MIN, FX_MIN, FX_MAX )), FX_RES_TO_DEGREES_FACTOR, FX_RES_MIN, DEGREES_MIN, DEGREES_MAX );
+	}
+
+	static public float fx( float value ) {
+		return (float)Math.exp(value);
+	}
+
 	boolean paused = false;
 	boolean canPress = false;
 	
@@ -68,17 +99,31 @@ public class ShadowsView extends GLBase {
 		}
 
 		public void onSensorChanged(int sensor, float[] values) {
-			if (!paused){
-				//Azimuth - z				
-				rquad = values[0];
-				//Pitch - x
-				xrot = values[1];
-				//Roll - y
-				yrot = values[2];	
+			synchronized (this) {
+				if (!paused){
+					//Azimuth - z				
+					rquad = values[0];
+					//Pitch - x
+					xrot = values[1];
+					//Roll - y
+					yrot = values[2];	
+	
+					//Inclinamos la x un poco, porque es la forma natural del
+					//teléfono en la mano
+					xrot += 15.0f;
+					if ( xrot > 90.0f )
+						xrot = 90.0f;
+					
+					//Aplicamos una conversión a los grados de rotación x e y 
+					//a través de una curva exponencial para que la inclinación
+					//no sea lineal
+					//No se si sirve para algo pero me parecía interesante hacerlo ;-)
+					xrot = softenDegrees(Math.abs(xrot))*Math.signum(xrot);
+					yrot = softenDegrees(Math.abs(yrot))*Math.signum(yrot);
+				}
 			}
-		}
-    	
-    };
+	    }
+	};
 	
 	public ShadowsView(Context c) {
 		super(c, 5);
@@ -113,8 +158,14 @@ public class ShadowsView extends GLBase {
 		super.end(gl);		
 	}
 
+
+
 	@Override
-	protected void init(GL10 gl){		
+	protected GL10 init(GL10 gl){
+
+		MatrixTrackingGL mgl = new MatrixTrackingGL(gl);
+		gl = (GL10)mgl;
+
 		// Smooth shading
 		gl.glShadeModel(GL10.GL_SMOOTH);	// Enables Smooth shading
 		
@@ -139,7 +190,33 @@ public class ShadowsView extends GLBase {
 		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_AMBIENT, lightAmbient,	0);
 		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_DIFFUSE, lightDiffuse,	0);
 		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, lightPosition, 0);
+		
+ 
+		//Los textos a pintar hay que crearlos primero usando un Paint, 
+		//para que se carguen como texturas 2D a las que haremos 
+		//referencia después
+        mLabelPaint = new Paint();
+        mLabelPaint.setTextSize(32);
+        mLabelPaint.setAntiAlias(true);
+        mLabelPaint.setARGB(0xff, 0x00, 0x00, 0x00);
+
+        if (mLabels != null) {
+            mLabels.shutdown(gl);
+        } else {
+            mLabels = new LabelMaker(true, 256, 256);
+        }
+        mLabels.initialize(gl);
+        mLabels.beginAdding(gl);
+        mLabelTest = mLabels.add(gl, "Texto OpenGL", mLabelPaint);
+        mLabelSW = mLabels.add(gl, "SW", mLabelPaint);
+        mLabelNE = mLabels.add(gl, "NE", mLabelPaint);
+        mLabelSE = mLabels.add(gl, "SE", mLabelPaint);
+        mLabelNW = mLabels.add(gl, "NW", mLabelPaint);
+        mLabels.endAdding(gl);
+        
+        return mgl;
 	}
+
 	
 	@Override
 	protected void drawFrame(GL10 gl) {				
@@ -152,19 +229,30 @@ public class ShadowsView extends GLBase {
 		
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);	// Clear The Screen And The Depth Buffer
 		gl.glMatrixMode(GL10.GL_MODELVIEW);		
-
 		gl.glColor4f(1.0f,1.0f,1.0f, 0.0f);				// Set The Color
-		
 		gl.glLoadIdentity();					// Reset The View, loading the identity matrix
 		gl.glTranslatef(0,0,-5); // center scene
-		
-		gl.glRotatef(xrot, 1.0f, 0, 0);			
-		gl.glRotatef(rquad,0.0f,0.0f,1.0f);	// rotate scene on z axis			
-				
+
+		//Pintamos los textos en la x,y de pantalla que queramos
+		//El origen de coordenadas es la esquina inferior izquierda
+		//La Z se utiliza para saber el orden de pintado. Si es >= 0
+		//se pinta sobre la perspectiva que estamos haciendo
+		mLabels.beginDrawing(gl, mWidth, mHeight);
+        mLabels.draw(gl, (mWidth-mLabels.getWidth(mLabelTest))/2, mHeight-mLabels.getHeight(mLabelTest), 0, mLabelTest);
+        mLabels.endDrawing(gl);
+
 		// textured quad
 		gl.glPushMatrix(); 
 			gl.glEnable(GL10.GL_TEXTURE_2D);						// Enable Texture Mapping 
 						
+			//Rotación de la brújula en los 3 ejes
+			synchronized (this) {
+				//Obtenemos la matriz de rotación simultanea en los 3 ejes
+				Utils.quatToMatrix( qm, 0, Utils.eulerToQuat((float)(-xrot*Math.PI/180.0), (float)(yrot*Math.PI/180.0), (float)(rquad*Math.PI/180.0)) );
+				//La aplicamos al modelo multiplicandola con OpenGL
+				gl.glMultMatrixf(qm, 0);
+			}
+
 			// textura a aplicar
 			gl.glBindTexture(GL10.GL_TEXTURE_2D, compassTex);
 		
@@ -179,6 +267,13 @@ public class ShadowsView extends GLBase {
 			// draw!
 			gl.glNormal3f(0,0,1.0f);
 			gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);		
+
+			mLabels.beginDrawing(gl, mWidth, mHeight);
+	        mLabels.draw(gl, quad[0], quad[1], quad[2], mLabelSW, 0);
+	        mLabels.draw(gl, quad[3], quad[4], quad[5], mLabelSE, 0);
+	        mLabels.draw(gl, quad[6], quad[7], quad[8], mLabelNW, 0);
+	        mLabels.draw(gl, quad[9], quad[10], quad[11], mLabelNE, 0);
+	        mLabels.endDrawing(gl);
 		gl.glPopMatrix();
 		
 		// prueba de circle
@@ -189,13 +284,34 @@ public class ShadowsView extends GLBase {
 		
 		// poste
 		gl.glPushMatrix();
+			gl.glLoadIdentity();					// Reset The View, loading the identity matrix
+			gl.glTranslatef(0,0,-5); // center scene
+
+			//Rotación del poste en x e y
+			synchronized (this) {
+				//Obtenemos la matriz de rotación simultanea en los ejes x, y
+				Utils.quatToMatrix( qm, 0, Utils.eulerToQuat((float)(-xrot*Math.PI/180.0), (float)(yrot*Math.PI/180.0), 0) );
+				//La aplicamos al modelo multiplicandola con OpenGL
+				gl.glMultMatrixf(qm, 0);
+			}
+			
 			gl.glColor4f(0, 0, 1, 1.0f);
 			gl.glTranslatef(0, 0, 0.5f);
 			gl.glScalef(0.25f, 0.25f, 1.0f);
 			drawPoste(gl);
-		gl.glPopMatrix();		
+		gl.glPopMatrix();
+
 	}
+
+	int mWidth;
+	int mHeight;
 	
+	protected void resize(GL10 gl, int w, int h) {
+		super.resize(gl, w, h );
+		mWidth = w;
+		mHeight= h;
+	}
+
 	public void drawPoste(GL10 gl){
 		float box[] = new float[] {
 				// FRONT
@@ -248,5 +364,4 @@ public class ShadowsView extends GLBase {
 		gl.glNormal3f(0,-1,0);
 		gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 20, 4);
 	}
-
 }
