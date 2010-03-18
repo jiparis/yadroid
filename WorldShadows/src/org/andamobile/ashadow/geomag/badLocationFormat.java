767328b4c3c735339596218d3ba7da996f49bb06
jiparis <jiparis@25cf9160-3d47-0410-9de4-f1fe730f28bb>
2010-03-18 01:44:26 +0000

    Refactorizacion

---
+++ WorldShadows/src/org/andamobile/ashadow/geomag/badLocationFormat.java
@@ -0,0 +1,21 @@
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   badLocationFormat.java
+
+package org.jiagjl.geomag;
+
+
+@SuppressWarnings("serial")
+public class badLocationFormat extends RuntimeException
+{
+
+    public badLocationFormat()
+    {
+    }
+
+    public badLocationFormat(String s)
+    {
+        super(s);
+    }
+}
